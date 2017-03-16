7417bf73ed61697969aeff6157cfbb0e6b7761ae
Zach Chrystall <z.chrystall@gmail.com>
2017-03-16 11:09:06 +1100

    creatd bare-bones login page and set up sessions

--- main.rb
+++ main.rb
@@ -1,11 +1,60 @@
-     
+
 require 'sinatra'
+require 'sinatra/reloader'
+require "pry"
+require "active_record"
+require_relative 'database_config'
+require_relative "models/comment"
+require_relative "models/group"
+require_relative "models/user"
+require_relative "models/interest"
+require_relative "models/user_group"
+require_relative "models/user_interest"
+require_relative "models/location"
+
+enable :sessions
+# before_filter :require_login
+
+# private
+#   def require_login
+#     unless current_user
+#       redirect :login
+#     end
+#   end
+# end
+
+helpers do
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?  # should return a boolean
+    !!current_user
+  end
+
+
+end
 
 get '/' do
   erb :index
 end
 
+get '/session/new' do
+  erb :login
+end
 
+post '/session' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    erb :login
+  end
+end
 
-
-
+delete '/session' do
+  session[:user_id] = nil
+  redirect '/session/new'
+end
--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :user_interests
